1601cee1b610cf7af1a2fc74113d152c9fa785f2
Sodynoizz_TH <tepza5556789@gmail.com>
2023-01-28 16:44:06 +0700

    ✅ Add newline at then end of file and fix grammar

--- config/secret[config].py
+++ config/secret[config].py
@@ -1,7 +1,7 @@
 covid_api = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
-database_name = "" # Your postgresql's database name
-database_password = "" # Your postgressql's database password
-database_user = "" # Your postgresql's user name
+database_name = "" # Your postgresql's database name here
+database_password = "" # Your postgressql's database password here
+database_user = "" # Your postgresql's user name here
 default_prefix = "" # Default prefix here
 initial_extensions = ['cogs.error', 'cogs.health', 'cogs.report', 'cogs.moderation', 'cogs.utility']
 report_channel = 1021746124018700298
